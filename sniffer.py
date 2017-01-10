eeee9e089dcf6fca81225105be2d795efc185942
tiancj <cj.tian.seu@gmail.com>
2017-01-10 16:36:25 +0800

    create raw socket in woker

--- sniffer.py
+++ sniffer.py
@@ -2,28 +2,59 @@
 
 import sys
 import getopt
+import socket
+import struct
+import fcntl
+import eloop
+
+ETH_P_ALL = 0x0003
+SIOCGIFINDEX = 0x8933
 
 
 class Sniffer(object):
     def __init__(self):
         self.workers = []
+        self.eloop = eloop.EventLoop()
 
     def add_worker(self, worker):
         self.workers.append(worker)
+
+    def start(self):
+        for w in self.workers:
+            w.init()
+        self.eloop.run()
 
 
 class SnifferWorker(object):
     def __init__(self, sniffer, ifname = None):
         self.ifname = ifname
+        self.fd = -1
         sniffer.add_worker(self)
-        pass
+        self.eloop = sniffer.eloop
 
     def __str__(self):
         return 'SnifferWorker: <ifname %s>' % self.ifname
 
+    def create_raw_socket(self):
+        self.fd = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
+        # step1: get the index of the interface
+        ret = fcntl.ioctl(self.fd, SIOCGIFINDEX, struct.pack('=6sI', bytes(self.ifname, 'ascii'), 0))
+        tmp, ifindex = struct.unpack('=6sI', ret)
+        print("ifname %s: ifindex %d" % (self.ifname, ifindex))
+
+        self.fd.bind((self.ifname, ETH_P_ALL))
+
+    def on_raw_packet_received(self, fd, mask):
+        print('packet on <%s> received\n' % self.ifname)
+
+    def init(self):
+        self.create_raw_socket()
+        self.eloop.register(self.fd, eloop.EVENT_READ, self.on_raw_packet_received)
+
 
 def usage(program):
     print("Usage: %s [-i <ifname>]\n", program)
+
 
 def main():
     sniffer = Sniffer()
@@ -40,6 +71,7 @@ def main():
 
     for w in sniffer.workers:
         print(w)
+    sniffer.start()
 
 
 if __name__ == '__main__':
